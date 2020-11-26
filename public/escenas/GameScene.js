981fb3dcde63a6e13ed05c149ec207a0b6dcde75
Luis Evilla <luisevilla588@gmail.com>
2020-11-26 15:23:46 -0500

    creacion de personajes con socket.io, y sus respectivos metodos

--- public/escenas/GameScene.js
+++ public/escenas/GameScene.js
@@ -14,11 +14,13 @@ export default class GameScene extends Phaser.Scene {
     this.scoreText = "";
     this.obstaculos = null;
     this.gameOver = false;
+    this.personajeCreado = false;
 
     // acumulador de tiempo entre cada actualización
     this.fps = 0;
     this.acumulador = 0;
     this.socket = socket;
+    this.id;
   }
 
   preload() {
@@ -26,8 +28,13 @@ export default class GameScene extends Phaser.Scene {
     this.load.image("suelo", "assets/fondos/Borde.png");
     this.load.image("obstaculo", "assets/star.png");
     this.load.spritesheet(
-      "personaje",
+      "personaje1",
       "assets/personajes/principal/principal.png",
+      { frameWidth: 64, frameHeight: 64 }
+    );
+    this.load.spritesheet(
+      "personaje2",
+      "assets/personajes/principal/Principal2.png",
       { frameWidth: 64, frameHeight: 64 }
     );
   }
@@ -42,14 +49,23 @@ export default class GameScene extends Phaser.Scene {
     this.platforms = this.physics.add.staticGroup();
 
     this.platforms.create(300, 423, "suelo").setScale(1.3, 1).refreshBody();
+
     //añadir personaje
-    this.player = this.physics.add
-      .sprite(100, 270, "personaje")
-      .setScale(1.5, 1.5)
-      .refreshBody();
-    this.player.setBounce("0.2");
-    this.player.setCollideWorldBounds(true);
-    this.animarPersonaje();
+    this.players = this.physics.add.group();
+
+    this.socket.emit("nuevoJugador", this.socket.id);
+
+    this.socket.on("actualizarJugadores", (data) => {
+      this.crearPersonaje(data);
+    });
+
+    this.socket.on("contagio", (id) => {
+        this.players.children.iterate(jugador => {
+            if (jugador.id == id) {
+                this.contagio(jugador);
+            }
+        })
+    });
 
     // contador de score
     this.scoreText = this.add.text(16, 16, "score: 0", {
@@ -62,21 +78,13 @@ export default class GameScene extends Phaser.Scene {
     });
 
     //colisiones entre suelo y personaje
-    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.players, this.platforms);
 
     // observando cuando las teclas son presionadas
     this.cursors = this.input.keyboard.createCursorKeys();
 
     // añadiendo el grupo de obstaculos
     this.obstaculos = this.physics.add.group();
-
-    // pool
-    this.obstaculosPool = this.add.group({
-      // Cuando la plataforma es removida del pool, se añade a las plataformas activas
-      removeCallback: function (obstaculo) {
-        obstaculo.scene.obstaculos.add(obstaculo);
-      },
-    });
 
     this.obstaculos.createMultiple({
       key: "obstaculo",
@@ -88,44 +96,99 @@ export default class GameScene extends Phaser.Scene {
       },
     });
 
-    this.obstaculos.setVelocityX(-180);
+    // this.obstaculos.setVelocityX(-180);
 
     this.physics.add.overlap(
       this.obstaculos,
-      this.player,
+      this.players,
       () => {
-          this.socket.emit('contagio')
+        this.socket.emit("contagio");
       },
       null,
       this
     );
 
-    this.socket.on('contagio', (id) => {
-        if (id == this.socket.id) {
-            this.contagio(this.player, this.obstaculos)
-        }
-    })
     this.physics.add.collider(this.platforms, this.obstaculos);
   }
 
-  contagio(player, obstaculo) {
-    this.gameOver = true;
-    player.setTint(0xff0000);
-    player.anims.play("turn");
-    player.setVelocityY(-300);
-    player.setVelocityX(0);
-    this.obstaculos.setVelocityX(0);
+  // crear los personajes a medida que se van conectando
+  crearPersonaje(data) {
+    var jugadores = data;
+    console.log(data);
+    var cont = 1;
 
-    this.time.addEvent({
-      delay: 1500,
-      callback: () => {
-        this.scene.stop();
-        this.game.scene.start("gameOver", this.score);
-      },
-      loop: false,
+    jugadores.forEach((jugador) => {
+      if (jugador != this.socket.id) {
+        var nuevoJugador = this.physics.add
+          .sprite(100, 270, `personaje${cont}`)
+          .setScale(1.5, 1.5)
+          .refreshBody();
+        nuevoJugador.setBounce("0.2");
+        nuevoJugador.id = jugador;
+        
+        this.players.add(nuevoJugador);
+        nuevoJugador.setCollideWorldBounds(true);
+        nuevoJugador.prsonaje = jugador.personaje;
+        nuevoJugador.vivo = true;
+        cont ++;
+    } else {
+        if (!this.personajeCreado) {
+            console.log("creando principal");
+          this.player = this.physics.add
+            .sprite(100, 270, `personaje${cont}`)
+            .setScale(1.5, 1.5)
+            .refreshBody();
+          this.player.setBounce("0.2");
+          this.player.id = this.socket.id;
+          this.animarPersonaje();
+          this.moverPersonaje();
+          this.players.add(this.player);
+
+          this.player.setCollideWorldBounds(true);
+
+          this.personajeCreado = true;
+          this.player.personaje = jugador.personaje 
+          this.player.vivo = true;
+          cont++;
+        }
+      }
     });
   }
 
+  // se ejecuta cuando un jugador choca contra algun elemento infectado
+  contagio(player) {
+    if ( player.id == this.socket.id) {
+        this.gameOver = true;
+    }
+    player.setTint(0xff0000);
+    //player.anims.play("turn");
+    //player.setVelocityY(-300);
+    player.setVelocityX(0);
+    player.vivo = false;
+
+    var aux = true;
+
+    this.players.children.iterate(jugador => {
+        if (jugador.vivo == true) {
+            aux = false;
+        }
+    })    
+
+    if (aux) {
+        this.time.addEvent({
+            delay: 1500,
+            callback: () => {
+              this.scene.stop();
+              this.game.scene.start("gameOver", this.score);
+            },
+            loop: false,
+          });
+    }
+
+    
+  }
+
+  // crea los obstaculos infectados
   crearObstaculo(posicionX) {
     var obstaculo;
 
@@ -138,8 +201,8 @@ export default class GameScene extends Phaser.Scene {
   //animacines de movimiento del personaje
   animarPersonaje() {
     this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("personaje", {
+      key: "left1",
+      frames: this.anims.generateFrameNumbers("personaje1", {
         start: 117,
         end: 125,
       }),
@@ -148,14 +211,14 @@ export default class GameScene extends Phaser.Scene {
     });
 
     this.anims.create({
-      key: "turn",
-      frames: [{ key: "personaje", frame: 26 }],
+      key: "turn1",
+      frames: [{ key: "personaje1", frame: 26 }],
       frameRate: 20,
     });
 
     this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("personaje", {
+      key: "right1",
+      frames: this.anims.generateFrameNumbers("personaje1", {
         start: 143,
         end: 151,
       }),
@@ -163,40 +226,80 @@ export default class GameScene extends Phaser.Scene {
       repeat: -1,
     });
 
+    this.anims.create({
+      key: "left2",
+      frames: this.anims.generateFrameNumbers("personaje2", {
+        start: 117,
+        end: 125,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn2",
+      frames: [{ key: "personaje2", frame: 26 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right2",
+      frames: this.anims.generateFrameNumbers("personaje2", {
+        start: 143,
+        end: 151,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+  }
+
+  // recibe los eventos de movimiento de los personajes
+  moverPersonaje() {
     this.socket.on("izquierda", (id) => {
-
-      if (id == this.socket.id) {
-        this.player.setVelocityX(-160);
-
-        this.player.anims.play("left", true);
-      }
-      
+        var cont = 1;
+        this.players.children.iterate(jugador => {
+            if (jugador.id == id) {
+                jugador.setVelocityX(-160);
+                jugador.anims.play(`left${cont}`)
+            }
+            cont++;
+        })
     });
 
     this.socket.on("derecha", (id) => {
+        var cont = 1;
 
-      if (id == this.socket.id) {
-        this.player.setVelocityX(160);
-
-        this.player.anims.play("right", true);
-      }
+        this.players.children.iterate((j) => {
+          if (j.id == id) {
+            j.setVelocityX(160);
+            j.anims.play(`right${cont}`);
+           } else {
+               cont++;
+           }
+        });
+      
     });
 
     this.socket.on("quieto", (id) => {
-
-      if (id == this.socket.id) {
-        this.player.setVelocityX(0);
-
-      this.player.anims.play("turn");
-      }
+        var cont = 1;
+       
+        this.players.children.iterate((j) => {
+          if (j.id == id) {
+            j.setVelocityX(0);
+            j.anims.play(`turn${cont}`);
+          } else {
+              cont++
+          }
+        });
       
     });
 
     this.socket.on("arriba", (id) => {
-
-        if (id == this.socket.id) {
-            this.player.setVelocityY(-250);
-        }
+        this.players.children.iterate((j) => {
+          if (j.id == id && j.body.touching.down) {
+            j.setVelocityY(-250);
+          } 
+        });
       
     });
   }
@@ -226,14 +329,14 @@ export default class GameScene extends Phaser.Scene {
 
     if (!this.gameOver) {
       if (this.cursors.left.isDown) {
-        this.socket.emit("izquierda");
+        this.socket.emit("izquierda", this.socket.id);
       } else if (this.cursors.right.isDown) {
-        this.socket.emit("derecha");
+        this.socket.emit("derecha", this.socket.id);
       } else {
-        this.socket.emit("quieto");
-      }
-      if (this.cursors.up.isDown && this.player.body.touching.down) {
-        this.socket.emit('arriba');
+        this.socket.emit("quieto", this.socket.id);
+      } 
+      if (this.cursors.up.isDown) {
+        this.socket.emit("arriba", this.socket.id);
       }
     }
   }
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const http = require('http');
-const socket = require('socket.io');
-const chalk = require('chalk');
-const logger = require('morgan');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+const chalk = require("chalk");
+const logger = require("morgan");
+const path = require("path");
+const { log } = require("console");
 
 // establece las variables de entorno, a partir del archivo .env
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 // setup del express
 const app = express();
 // app.use(logger('tiny'));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // servidor http
 const servidor = http.createServer(app);
@@ -23,40 +24,50 @@ const io = socket(servidor);
 
 const jugadores = [];
 
-io.on('connection', socket => {
-    jugadores.push(socket);
-    console.log(`Se ha conectado un nuevo usuario con ID: ${chalk.green(socket.id)}`);
-    
-    socket.on('contagio', () => {
-        socket.emit('contagio', socket.id);
-    })
 
-    socket.on('izquierda', () => {
-        socket.emit('izquierda', socket.id);
-    })
 
-    socket.on('derecha', () => {
-        socket.emit('derecha', socket.id);
-    })
+io.on("connection", (socket) => {
+  socket.on("nuevoJugador", id => {
+    jugadores.push(id);
+    console.log(
+      `Se ha conectado un nuevo usuario con ID: ${chalk.green(socket.id)}`
+    );
+    console.log(`número de jugadores actual: ${jugadores.length}`);
+    io.emit("actualizarJugadores", jugadores);
+  });
 
-    socket.on('arriba', () => {
-        socket.emit('arriba', socket.id);
-    })
+  socket.on("contagio", () => {
+    io.emit("contagio", socket.id);
+  });
 
-    socket.on('quieto', () => {
-        socket.emit('quieto', socket.id);
-    })
+  socket.on("izquierda", () => {
+    io.emit("izquierda", socket.id);
+  });
 
-    socket.on('disconnect', () => {
-        jugadores.pop(socket);
-        console.log(`Se ha desconectado un usuario con ID: ${chalk.red(socket.id)}`);
-    })
-})
+  socket.on("derecha", () => {
+    io.emit("derecha", socket.id);
+  });
 
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
+  socket.on("arriba", () => {
+    io.emit("arriba", socket.id);
+  });
+
+  socket.on("quieto", () => {
+    io.emit("quieto", socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    jugadores.pop(socket);
+    console.log(
+      `Se ha desconectado un usuario con ID: ${chalk.red(socket.id)}`
+    );
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 servidor.listen(PORT, (req, res) => {
-    console.log(`Server listenning on port ${chalk.green(PORT)}`);
-})
+  console.log(`Server listenning on port ${chalk.green(PORT)}`);
+});
